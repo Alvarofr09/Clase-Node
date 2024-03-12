069a6329f940938a3acd5512f9ae849bf56a2a61
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-12 12:59:51 +0100

    Hacerlo mismo con import

---
+++ Ejercicios/01_modulos_nodejs/01_2_mjs/index.mjs
@@ -0,0 +1,8 @@
+// ES Modules import
+
+//.js -> por defecto utiliza CommonJS
+//.mjs -> para utilizar ES Modules
+//.cjs -> para utilizar CommonJS
+import { sum } from "./math.mjs";
+
+console.log(sum(1, 2));
