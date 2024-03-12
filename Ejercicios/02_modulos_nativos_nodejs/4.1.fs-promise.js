d3c0a86a9a26e1c55d5081fc3a59be2593220128
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-12 13:48:03 +0100

    Leer archivo con promesas

---
+++ Ejercicios/02_modulos_nativos_nodejs/4.1.fs-promise.js
@@ -0,0 +1,13 @@
+const fs = require("node:fs/promises");
+
+console.log("leyendo el primer archivo...");
+fs.readFile("./archivo.txt", "utf-8").then((data) => {
+	console.log("primer archivo:", data);
+});
+
+console.log(".....haciendo cosas mientras leemos el archivo...");
+
+console.log("leyendo el segundo archivo...");
+fs.readFile("./archivo2.txt", "utf-8").then((data) => {
+	console.log("segundo archivo:", data);
+});
---
+++ Ejercicios/02_modulos_nativos_nodejs/4.2.fs-async-await.js
@@ -0,0 +1,13 @@
+const { readFile } = require("node:fs/promises");
+
+(async () => {
+	console.log("leyendo el primer archivo...");
+	const text = await readFile("./archivo.txt", "utf-8");
+	console.log("primer archivo:", text);
+
+	console.log(".....haciendo cosas mientras leemos el archivo...");
+
+	console.log("leyendo el segundo archivo...");
+	const text2 = await readFile("./archivo2.txt", "utf-8");
+	console.log("segundo archivo:", text2);
+})();
