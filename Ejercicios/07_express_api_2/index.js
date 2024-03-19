8e1fddb29524c1ed7b4decda83cb840c03e95f76
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-19 13:08:06 +0100

    importamos el archivo accountRouter

--- Ejercicios/07_express_api_2/index.js
+++ Ejercicios/07_express_api_2/index.js
@@ -2,6 +2,8 @@
 const express = require("express");
 // Importamos dotenv
 const dotenv = require("dotenv");
+// Importamos accountRouter
+const accountRouter = require("./routes/accountRouter");
 
 // Cargamos variables de entorno
 dotenv.config();
@@ -13,6 +15,8 @@ const app = express();
 // middlewares para interpretar el formato JSON y text desde el cliente por http
 app.use(express.json());
 app.use(express.text());
+// middleware que hemos importado del router accountRouter
+app.use(accountRouter);
 
 // Levantamos el servidor en el puerto 3000
 app.listen(port, () => {
