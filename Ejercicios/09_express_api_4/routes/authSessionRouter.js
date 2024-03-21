e231d31f5a8cf5693ffea2f366a7e5eb0f9bbcfd
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-21 13:42:55 +0100

    espacio

--- Ejercicios/09_express_api_4/routes/authSessionRouter.js
+++ Ejercicios/09_express_api_4/routes/authSessionRouter.js
@@ -59,7 +59,7 @@ authSessionRouter.get("/profile", (req, res) => {
 	// Si no obtenemos el usuario enviamos un 401 (unauthorized)
 	if (!user) return res.sendStatus(401);
 
-	// Borramos la contraseñadel objeto obtenido para no mostarlo
+	// Borramos la contraseña del objeto obtenido para no mostarlo
 	delete userSession.password;
 
 	// Y devolvemos los datos del usuario
