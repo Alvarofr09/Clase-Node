769eb55b318bd505bf30f45eeebbbb4d030b6042
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-03-21 11:48:28 +0100

    copiar la carpeta anterior

---
+++ Ejercicios/09_express_api_4/index.js
@@ -0,0 +1,32 @@
+// Importamos express
+const express = require("express");
+// Importamos dotenv
+const dotenv = require("dotenv");
+// Importamos Morgan (middleware de peticiones HTTP para node.js)
+const logger = require("morgan");
+// Importamos accountRouter
+const accountRouter = require("./routes/accountRouter");
+const authRouter = require("./routes/authRouter");
+
+// Cargamos variables de entorno
+dotenv.config();
+
+// Definimos el puerto
+const port = process.env.PORT;
+const app = express();
+
+// middlewares para interpretar el formato JSON y text desde el cliente por http
+app.use(express.json());
+app.use(express.text());
+// middleware de peticiones HTTP para node.js (Morgan)
+app.use(logger("dev"));
+
+// middleware que hemos importado del router accountRouter
+app.use("/account", accountRouter);
+// middleware que hemos importado del router authRouter
+app.use("/auth", authRouter);
+
+// Levantamos el servidor en el puerto 3000
+app.listen(port, () => {
+	console.log(`Server levantado en el puerto ${port}`);
+});
