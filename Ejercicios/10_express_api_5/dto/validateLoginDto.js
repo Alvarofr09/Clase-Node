a72d7cf19e99b534d8683373e496d664aecf9e5e
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-01 13:40:17 +0200

    Usar los formatos y los errores personalizados

--- Ejercicios/10_express_api_5/dto/validateLoginDto.js
+++ Ejercicios/10_express_api_5/dto/validateLoginDto.js
@@ -3,20 +3,47 @@ const { Type } = require("@sinclair/typebox");
 
 // Importamos libreria ajv
 const Ajv = require("ajv");
-
-// Instanciamos la clase
-const ajv = new Ajv();
+const addFormats = require("ajv-formats");
+const addErrors = require("ajv-errors");
 
 // Componemos el esquema usando la libreria con la propiedad type
 const loginDTOSchema = Type.Object(
 	{
-		email: Type.String(),
-		password: Type.String(),
+		email: Type.String({
+			format: "email",
+			errorMessage: {
+				type: " El tipo de email debe ser un string",
+				format: "El email debe contener un correo electronico valido",
+			},
+		}),
+		password: Type.String({
+			errorMessage: {
+				type: " El tipo de password debe ser un string",
+			},
+		}),
 	},
 	{
 		additionalProperties: false,
+		errorMessage: {
+			type: "Debe ser un objeto",
+			additionalProperties: "El formato del bojeto no es valido",
+			required: {
+				email: "El campo email es requerido",
+				password: "El campo password es requerido",
+			},
+		},
 	}
 );
+
+// Instanciamos la clase
+const ajv = new Ajv({ allErrors: true });
+
+// Funciones para extender la funcionalidad de Ajv
+// addFormats añade formato de validación (lo aplicamos al email)
+addFormats(ajv, ["email"]);
+
+// addErrors permite personalizar los errores
+addErrors(ajv, { keepErrors: false });
 
 // Metemos el esquema en el ajv para generar una funcion de validacion
 const validate = ajv.compile(loginDTOSchema);
@@ -26,7 +53,10 @@ const validateLoginDto = (req, res, next) => {
 	const isDTOValid = validate(req.body);
 
 	// Si no ha pasado la validacion enviamos un 400 (bad request)
-	if (!isDTOValid) return res.status(400).send("El body no es valido");
+	if (!isDTOValid)
+		return res
+			.status(400)
+			.send(ajv.errorsText(validate.errors, { separator: "\n" }));
 
 	next();
 };
