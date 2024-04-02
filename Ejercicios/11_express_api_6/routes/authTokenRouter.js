0e543511b5a07e662df9615269e724e8ec592212
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-02 12:59:32 +0200

    controladores de authToken

--- Ejercicios/11_express_api_6/routes/authTokenRouter.js
+++ Ejercicios/11_express_api_6/routes/authTokenRouter.js
@@ -1,84 +1,15 @@
 const express = require("express");
-const { SignJWT, jwtVerify } = require("jose");
 
-const { USERS_BBDD } = require("../bbdd");
-const checkEmailPassword = require("../utils/checkEmailPassword");
 const validateLoginDto = require("../dto/validateLoginDto");
+const {
+	loginToken,
+	getProfileToken,
+} = require("../controllers/authTokenController");
 
 const authTokenRouter = express.Router();
 
-authTokenRouter.post("/login", validateLoginDto, async (req, res) => {
-	//Obtenemos el email y password del body
-	const { email, password } = req.body;
-	// Si no existe alguno de esos dos campos devolvemos un 400 (bad request)
-	if (!email || !password) return res.sendStatus(400);
+authTokenRouter.post("/login", validateLoginDto, loginToken);
 
-	try {
-		// Validamos el email y password y obtenemos el guid
-		const { guid } = await checkEmailPassword(email, password);
-
-		// GENERAR TOKEN Y DEVOLVER TOKEN
-		// Construimos el JWT con el guid
-		const jwtConstructor = new SignJWT({ guid });
-
-		// Codificamos la clave secreta definida en la variable de entorno por requisito de la libreria jose
-		// y poder pasarla en el formato correcto (uint8Array) en el metodo .sign
-		const encoder = new TextEncoder();
-
-		// Generamos el JWT. Lo hacemos asincrono, ya que nos devuelve una promesa.
-		// Le indicamos la cabecera, la creacion, la expiracion y la firma (clave secreta)
-		const jwt = await jwtConstructor
-			.setProtectedHeader({ alg: "HS256", typ: "JWT" })
-			.setIssuedAt()
-			.setExpirationTime("1h")
-			.sign(encoder.encode(process.env.JWT_SECRET));
-
-		// Si todo es correcto enviamos la respuesta. 200 (ok)
-		return res.send({ jwt });
-	} catch (error) {
-		console.log(error);
-
-		// Si el usuario no existe devolvemos un 401 (unauthorized)
-		return res.sendStatus(401);
-	}
-});
-
-authTokenRouter.get("/profile", async (req, res) => {
-	// OBTENER CABECERA Y COMPROBAR SU AUTENTICIDAD Y CADUCIDAD
-	const { authorization } = req.headers;
-
-	// Si no existe el token enviamos un 401 (unauthorized)
-	if (!authorization) return res.sendStatus(401);
-
-	try {
-		const token = authorization.split(" ")[1];
-
-		// Codificamos la clave secreta
-		const encoder = new TextEncoder();
-
-		// Verficamos el token con la funcion jwtVerify. Le pasamos el token y la clave secreta
-		const { payload } = await jwtVerify(
-			token,
-			encoder.encode(process.env.JWT_SECRET)
-		);
-
-		// Obtenemos los datos del usuario a traves del guid
-		const user = USERS_BBDD.find((user) => user.guid === payload.guid);
-
-		// Si no obtenemos el usuario enviamos un 401 (unauthorized)
-		if (!user) return res.sendStatus(401);
-
-		// Borramos la password del objeto obtenido para no mostarla
-		delete user.password;
-
-		// Y devolvemos los datos del usuario
-		return res.send(user);
-	} catch (error) {
-		console.log(error);
-
-		// Si hay un error o el token expiro enviamos un 401 (unauthorized)
-		return res.sendStatus(401);
-	}
-});
+authTokenRouter.get("/profile", getProfileToken);
 
 module.exports = authTokenRouter;
