11cf389d10acd544cb0f4d550a50fd15a3139c68
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-02 12:45:47 +0200

    controladores de auth

--- Ejercicios/11_express_api_6/routes/authRouter.js
+++ Ejercicios/11_express_api_6/routes/authRouter.js
@@ -1,54 +1,22 @@
 // importamos express
 const express = require("express");
-// Importamos bbdd
-const { USERS_BBDD } = require("../bbdd.js");
-// Importamos checkEmailPassword
-const checkEmailPassword = require("../utils/checkEmailPassword");
+
+// Importamos los controladores
+const {
+	getPublic,
+	login,
+	loginAutorizado,
+} = require("../controllers/authController.js");
 
 const authRouter = express.Router();
 
 // Endpoint público (no autenticado y no autorizado)
-authRouter.get("/public", (req, res) => res.send("Endpoint publico"));
+authRouter.get("/public", getPublic);
 
 // Endpoint autenticado para todo usuario registrado
-authRouter.post("/autenticado", async (req, res) => {
-	// Obtenemos el email y el password del body
-	const { email, password } = req.body;
-
-	// Si no existe alguno de estos campos devolvemos un 400 (bad request)
-	if (!email || !password) return res.sendStatus(400);
-
-	try {
-		// Llamamos a la funcion de validar el email y password
-		const user = await checkEmailPassword(email, password);
-
-		// Si todo sale bien respondemos con un 200 (ok)
-		return res.send(`Usuario ${user.name} autenticado`);
-	} catch (error) {
-		return res.sendStatus(401);
-	}
-});
+authRouter.post("/autenticado", login);
 
 // Endpoint autorizado a administradores
-authRouter.post("/autorizado", async (req, res) => {
-	// Obtenemos el email y el password del body
-	const { email, password } = req.body;
-
-	// Si no existe alguno de estos campos devolvemos un 400 (bad request)
-	if (!email || !password) return res.sendStatus(400);
-
-	try {
-		// Llamamos a la funcion de validar el email y password
-		const user = await checkEmailPassword(email, password);
-
-		// Si el rol del usuario no es administrador devolvemos un 403 (forbidden)
-		if (user.role !== "Admin") return res.sendStatus(403);
-
-		// Si todo sale bien respondemos con un 200 (ok)
-		return res.send(`Usuario ${user.name} autenticado`);
-	} catch (error) {
-		return res.sendStatus(401);
-	}
-});
+authRouter.post("/autorizado", loginAutorizado);
 
 module.exports = authRouter;
