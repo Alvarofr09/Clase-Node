2bb974ab4ca63afd29eee724bcb699afe5a420ee
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-03 13:59:38 +0200

    comentar

--- Ejercicios/12_mongodb_express_api_01/services/schemas/userSchema.js
+++ Ejercicios/12_mongodb_express_api_01/services/schemas/userSchema.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.Schema({
+const userSchema = mongoose.Schema({
 	_id: String,
 	name: String,
 	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
-	role: String,
+	// password: { type: String, required: true },
+	// role: String,
 });
 
 const userModel = mongoose.model("User", userSchema);
 
-module.exports = userSchema;
+module.exports = userModel;
