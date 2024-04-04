bb35ca2f61a83d7ccb8dc879b2b7306a79215874
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-04 11:54:30 +0200

    poner contraseña y rol

--- Ejercicios/12_mongodb_express_api_01/services/schemas/userSchema.js
+++ Ejercicios/12_mongodb_express_api_01/services/schemas/userSchema.js
@@ -4,8 +4,8 @@ const userSchema = mongoose.Schema({
 	_id: String,
 	name: String,
 	email: { type: String, required: true, unique: true },
-	// password: { type: String, required: true },
-	// role: String,
+	password: { type: String, required: true },
+	role: String,
 });
 
 const userModel = mongoose.model("User", userSchema);
