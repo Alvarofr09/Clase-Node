c77a304861bf592a63bdc6457386377f569a6901
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-08 13:40:25 +0200

    logica del registro

--- Ejercicios/13_mysql_express_api/controllers/userController.js
+++ Ejercicios/13_mysql_express_api/controllers/userController.js
@@ -1,10 +1,23 @@
-const addUser = (req, res) => {
-	const { email, password, name } = req.body;
+const dao = require("../services/dao/userDao");
 
-	if (!email || !password || !name)
+const addUser = async (req, res) => {
+	const { name, email, password } = req.body;
+
+	if (!name || !email || !password)
 		return res.status(400).send("Error en el body");
 
-	res.send("Añadir usuario");
+	// Buscamos el usuario en la base de datos
+	try {
+		const user = await dao.getUserByEmail(email);
+		// Si existe el usuario respondemos con un 409 (conflict)
+		if (user.length > 0) return res.status(409).send("Usuario ya registrado");
+		// Si no existe lo registramos
+		const userId = await dao.addUser(req.body);
+		if (userId) return res.send(`Usuario ${name} con id ${userId}, registrado`);
+	} catch (error) {
+		console.log(error.message);
+		throw new Error(error);
+	}
 };
 
 module.exports = {
