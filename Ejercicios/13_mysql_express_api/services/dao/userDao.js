bdffd48fa61103c18c499fd315e9ccc175eca730
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-08 12:35:32 +0200

    funcion getUserByEmail

--- Ejercicios/13_mysql_express_api/services/dao/userDao.js
+++ Ejercicios/13_mysql_express_api/services/dao/userDao.js
@@ -1,3 +1,23 @@
+const moment = require("moment");
+const md5 = require("md5");
+
 const db = require("../db");
 
-const dao = {};
+const userDao = {};
+
+userDao.getUserByEmail = async (email) => {
+	let conn = null;
+	try {
+		conn = await db.createConection();
+		return await db.query(
+			"SELECT * FROM users WHERE email = ?",
+			email,
+			"select",
+			conn
+		);
+	} catch (e) {
+		throw new Error(e);
+	} finally {
+		conn && (await conn.end());
+	}
+};
