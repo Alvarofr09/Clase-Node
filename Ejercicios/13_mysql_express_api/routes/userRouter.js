ab6c79f8013693ac0360587ff2079a425381c481
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-08 14:08:54 +0200

    añadir otro endpoint

--- Ejercicios/13_mysql_express_api/routes/userRouter.js
+++ Ejercicios/13_mysql_express_api/routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { addUser } = require("../controllers/userController");
+const { addUser, loginUser } = require("../controllers/userController");
 
 const userRouter = express.Router();
 
 userRouter.post("/", addUser);
+
+userRouter.post("/login", loginUser);
 
 module.exports = userRouter;
