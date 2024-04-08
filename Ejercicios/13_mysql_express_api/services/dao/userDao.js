68ab32a0441adafbe3b15d57a3406e806967927e
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-08 12:40:57 +0200

    funcion addUser

--- Ejercicios/13_mysql_express_api/services/dao/userDao.js
+++ Ejercicios/13_mysql_express_api/services/dao/userDao.js
@@ -21,3 +21,27 @@ userDao.getUserByEmail = async (email) => {
 		conn && (await conn.end());
 	}
 };
+
+userDao.addUser = async (userData) => {
+	// Conectamos con la base de datos y añadimos el usuario
+	let conn = null;
+	try {
+		conn = await db.createConection();
+		// Creamos un objeto con los datos del usuario a guardar en la base de datos
+		// Encriptamos la password con md5 y usamos la libreria momentjs para registrar la fecha actual
+		let userObj = {
+			name: userData.name,
+			surname: userData.surname,
+			email: userData.email,
+			password: md5(userData.password),
+			registerDate: moment().format("YYYY-MM-DD HH:mm:ss"),
+		};
+		return await db.query("INSERT INTO users SET ?", userObj, "insert", conn);
+	} catch (error) {
+		throw new Error(error);
+	} finally {
+		conn && (await conn.end());
+	}
+};
+
+module.exports = userDao;
