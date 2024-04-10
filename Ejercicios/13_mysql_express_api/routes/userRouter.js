fdca08a0fd40db538f28bf6d62605de406fd3b84
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-10 12:50:51 +0200

    Ruta de delete

--- Ejercicios/13_mysql_express_api/routes/userRouter.js
+++ Ejercicios/13_mysql_express_api/routes/userRouter.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { addUser, loginUser } = require("../controllers/userController");
+const {
+	addUser,
+	loginUser,
+	deleteUser,
+} = require("../controllers/userController");
 
 const userRouter = express.Router();
 
 userRouter.post("/", addUser);
-
 userRouter.post("/login", loginUser);
+
+userRouter.delete("/:id", deleteUser);
 
 module.exports = userRouter;
