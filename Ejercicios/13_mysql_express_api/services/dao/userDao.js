8ef9e69f3cfdaaec96715338a8eb1af36c5888e8
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-10 13:41:33 +0200

    funcion para actualizar

--- Ejercicios/13_mysql_express_api/services/dao/userDao.js
+++ Ejercicios/13_mysql_express_api/services/dao/userDao.js
@@ -2,6 +2,7 @@ const moment = require("moment");
 const md5 = require("md5");
 
 const db = require("../db");
+const removeUndefinedKeys = require("../../utils/removeUndefinedKeys");
 
 const userDao = {};
 
@@ -69,6 +70,35 @@ userDao.deleteUser = async (id) => {
 	} catch (error) {
 		console.log(error.message);
 		throw new Error(error);
+	} finally {
+		conn && (await conn.end());
+	}
+};
+
+userDao.updateUser = async (id, userData) => {
+	let conn = null;
+	try {
+		conn = await db.createConection();
+
+		let userObj = {
+			name: userData.name,
+			surname: userData.surname,
+			email: userData.email,
+			password: userData.password ? md5(userData.password) : undefined,
+			updateDate: moment().format("YYYY-MM-DD HH:mm:ss"),
+		};
+
+		// Eliminamos los campos que no se van a mofificar
+		userObj = await removeUndefinedKeys(userObj);
+
+		return await db.query(
+			"UPDATE users SET ? WHERE id = ?",
+			[userObj, id],
+			"update",
+			conn
+		);
+	} catch (e) {
+		throw new Error(e);
 	} finally {
 		conn && (await conn.end());
 	}
