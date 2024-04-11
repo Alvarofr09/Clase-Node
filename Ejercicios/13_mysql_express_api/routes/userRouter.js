2b410c0e0ba3d587fe8c2670709659cdd4440c59
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-11 13:59:55 +0200

    Hacer el endpoints de get

--- Ejercicios/13_mysql_express_api/routes/userRouter.js
+++ Ejercicios/13_mysql_express_api/routes/userRouter.js
@@ -4,15 +4,22 @@ const {
 	loginUser,
 	deleteUser,
 	updateUser,
+	getProfileUser,
 } = require("../controllers/userController");
 
 const userRouter = express.Router();
 
+/*----------- Endpoints de GET -----------*/
+userRouter.get("/:id", getProfileUser);
+
+/*----------- Endpoints de POST -----------*/
 userRouter.post("/", addUser);
 userRouter.post("/login", loginUser);
 
+/*----------- Endpoints de DELETE -----------*/
 userRouter.delete("/:id", deleteUser);
 
+/*----------- Endpoints de PATCH -----------*/
 userRouter.patch("/:id", updateUser);
 
 module.exports = userRouter;
