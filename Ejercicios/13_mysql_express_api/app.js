67c4a821221e0b251074a5f8c8c613d392c04fe8
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-12 14:02:56 +0200

    instanciamos la libreria express-fileupload

--- Ejercicios/13_mysql_express_api/app.js
+++ Ejercicios/13_mysql_express_api/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const dontenv = require("dotenv");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
@@ -10,6 +11,17 @@ dontenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+// Instanciamos la libreria express-fileupload (para subir archivos)
+app.use(
+	fileUpload({
+		createParentPath: true, // Crea la carpeta donde almacenamos las imagenes si no ha sido creada
+		limits: { fieldSize: 20 * 1024 * 1024 }, // Limitamos el tamaño de la imagen a 20mb
+		abortOnLimit: true, // Interrumpimos la subida de la imagen si excede el limite
+		responseOnLimit: "Imagen demasiado grande", // Enviaremos un mensaje de respuesta cuando se interrumpe la carga
+		uploadTimeout: 0, // Indicamos el tiempo de respuesta si se interrumpe la carga de la imagen
+	})
+);
 
 // middlewares
 app.use(express.json());
