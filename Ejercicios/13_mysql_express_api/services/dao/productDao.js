74e2083c6e8d8239e46b9772146d6f5a82ea2c85
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-12 14:03:15 +0200

    dao de addproduct y file

--- Ejercicios/13_mysql_express_api/services/dao/productDao.js
+++ Ejercicios/13_mysql_express_api/services/dao/productDao.js
@@ -1,12 +1,19 @@
+const { removeUndefinedKeys } = require("../../utils/removeUndefinedKeys");
+const moment = require("moment");
 const db = require("../db");
 
 const productDao = {};
 
-productDao.getAllProducts = async () => {
+productDao.getProductByReference = async (reference) => {
 	let conn = null;
 	try {
 		conn = await db.createConection();
-		return await db.query("SELECT * FROM products", null, "select", conn);
+		return await db.query(
+			"SELECT * FROM products WHERE reference = ?",
+			reference,
+			"select",
+			conn
+		);
 	} catch (error) {
 		throw new Error(error);
 	} finally {
@@ -31,17 +38,55 @@ productDao.getProductById = async (id) => {
 	}
 };
 
-productDao.addProducts = async (productData) => {
+productDao.addProduct = async (productData) => {
 	let conn = null;
 	try {
 		conn = await db.createConection();
+		// Creamos un objeto con los datos del producto a guardar en la bbdd
+		// Usamos la libreria momentjs para registrar la fecha actual
+		let productObj = {
+			name: productData.name,
+			description: productData.description,
+			reference: productData.reference,
+			stock: productData.stock,
+			price: productData.price,
+			registerDate: moment().format("YYYY-MM-DD HH:mm:ss"),
+		};
+
+		// Eliminamos los campos que no se van a registrar
+		productObj = removeUndefinedKeys(productObj);
+
+		// Insertamos el nuevo producto
 		return await db.query(
 			"INSERT INTO products SET ?",
-			productData,
+			productObj,
 			"insert",
 			conn
 		);
 	} catch (error) {
+		throw new Error(error);
+	} finally {
+		conn && (await conn.end());
+	}
+};
+
+productDao.addProductFile = async (fileData) => {
+	// Conectamos con la bbdd y añadimos la imagen
+	let conn = null;
+	try {
+		conn = await db.createConection();
+		// Creamos un objeto con los datos del fichero a guardar en la bbdd
+		let fileObj = {
+			name: fileData.name,
+			path: fileData.path,
+			productId: fileData.productId,
+			registerDate: moment().format("YYYY-MM-DD HH:mm:ss"),
+		};
+
+		// Instertamos el registro
+		return await db.query("INSERT INTO images SET ?", fileObj, "insert", conn);
+	} catch (error) {
+		console.log(error.mesagge);
 		throw new Error(error);
 	} finally {
 		conn && (await conn.end());
