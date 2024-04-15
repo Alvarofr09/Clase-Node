e9db917a505899fa7b084c98cb41f4a31423d9c7
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-15 13:12:06 +0200

    urlencoded

--- Ejercicios/13_mysql_express_api/app.js
+++ Ejercicios/13_mysql_express_api/app.js
@@ -27,6 +27,7 @@ app.use(
 app.use(express.json());
 app.use(express.text());
 app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // PETICIONES A NUESTRA API
